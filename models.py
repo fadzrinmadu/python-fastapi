bad56a0d7bc747c35f55dfa5bf2e277939521576
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2024-02-22 03:58:00 +0700

    module not found

--- models.py
+++ models.py
@@ -1,28 +1,27 @@
-from sqlalchemy import Column, Integer, Boolean, Float, ForeignKey
-from sqlalchemy.dialects.mysql import VARCHAR
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
 from database import Base
 
 class Users(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(VARCHAR(length=255), unique=True)
-    username = Column(VARCHAR(length=255), unique=True)
-    first_name = Column(VARCHAR(length=255))
-    last_name = Column(VARCHAR(length=255))
-    hashed_password = Column(VARCHAR(length=255))
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    role = Column(VARCHAR(length=255))
+    role = Column(String)
     
 
 class Books(Base):
     __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(VARCHAR(length=255))
-    author = Column(VARCHAR(length=255))
-    description = Column(VARCHAR(length=255))
-    category = Column(VARCHAR(length=255))
+    title = Column(String)
+    author = Column(String)
+    description = Column(String)
+    category = Column(String)
     rating = Column(Float)
     published_date = Column(Integer)
     is_read = Column(Boolean, default=False)
